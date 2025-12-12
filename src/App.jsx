06aca3bed6b766344b56a6516fc29f97f9286914
tasnim29 <tasnimmahmudnjr@gmail.com>
2025-12-12 11:49:02 +0600

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import gsap from "gsap";
 import "./App.css";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
