d369e4064418c546d4f872e371db1d0aab922840
tasnim29 <tasnimmahmudnjr@gmail.com>
2025-12-13 19:41:43 +0600

    about

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </>
   );
 }
